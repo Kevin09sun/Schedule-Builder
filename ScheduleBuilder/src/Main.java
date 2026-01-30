d1280d538d7f0651fabc31bc2bd384f6a0661c34
Hongyi Sun <kevin09.sun@gmail.com>
2026-01-30 11:38:45 -0500

    Schedule optimizer kinda works, the optimal schedule seems to be in reverse order tho

--- ScheduleBuilder/src/Main.java
+++ ScheduleBuilder/src/Main.java
@@ -12,17 +12,19 @@ public class Main {
         File f = new File(filePathLocation);
         LocationManager lm = new LocationManager();
         ConflictChecker cc = new ConflictChecker(lm);
+        ScheduleOptimizer so = new ScheduleOptimizer(cc);
         System.out.println("Hello world");
 
         DataManager.loadLoaction(filePathLocation, lm);
 
         ArrayList<User> allusers = DataManager.loadUsers(filePathUser);
         for (User u : allusers){
-            if (u.getActivities().size() >= 2){
-                if (cc.hasConflict(u.getActivities().get(0), u.getActivities().get(1))){
-                    System.out.println(u.getName() + " has conflicts");
-                }
+            System.out.println("Optimal Activities for user: " + u.getName());
+            ArrayList<Activity> temp = so.getOptimalSchedule(u.getActivities());
+            for (Activity a : temp){
+                System.out.print(a.getName() + " ");
             }
+            System.out.println();
         }
     }
 }
--- ScheduleBuilder/src/logic/ScheduleOptimizer.java
+++ ScheduleBuilder/src/logic/ScheduleOptimizer.java
@@ -1,6 +1,55 @@
 package ScheduleBuilder.src.logic;
-
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.lang.reflect.Array;
+import java.util.*;
+import ScheduleBuilder.src.data.*;
+import ScheduleBuilder.src.model.*;
 public class ScheduleOptimizer {
+    private ConflictChecker cc;
+    public ScheduleOptimizer(ConflictChecker cc){
+        this.cc = cc;
+    }
 
-    
+    public ArrayList<Activity> getOptimalSchedule(ArrayList<Activity> allActivities){
+        Collections.sort(allActivities, Comparator.comparingInt(Activity::getStartTime));
+        return recurse(allActivities, 0, null);
+    }
+
+    private ArrayList<Activity> recurse(ArrayList<Activity> activities, int currentIndex, Activity last){
+        if (currentIndex >= activities.size()){
+            return new ArrayList<>();
+        }
+
+        Activity a = activities.get(currentIndex);
+        ArrayList<Activity> skip = recurse(activities, currentIndex + 1, last);
+        int skipScore = calculate(skip);
+
+        ArrayList<Activity> include = new ArrayList<>();
+        int includeScore = -1;
+        boolean conflict = false;
+        if (last != null){
+            conflict = cc.hasConflict(last, a);
+        }
+        if (!conflict){
+            include = recurse(activities, currentIndex + 1, a);
+            include.add(a);
+            includeScore = calculate(include);
+        }
+        if (skipScore > includeScore){
+            return skip;
+        }
+        else {
+            return include;
+        }
+    }
+
+    private int calculate(ArrayList<Activity> activites){
+        int sum = 0;
+        for (Activity a : activites){
+            sum += a.getPriority();
+        }
+        return sum;
+    }
+
 }
